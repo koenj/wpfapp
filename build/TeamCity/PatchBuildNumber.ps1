82ac33e824182f8cd32ac91a209277181d4e6a64
Koen Jans <koen.jans@ipasoftware.com>
2017-07-02 13:24:21 +0200

    Script to patch build nb

---
+++ build/TeamCity/PatchBuildNumber.ps1
@@ -0,0 +1,36 @@
+# Parse the BuildNumber from the AssemblyInfo.Shared.cs file
+$assemblyInfoPath = '.\src\AssemblyInfo.Shared.cs'
+if (-Not(Test-Path($assemblyInfoPath))) {
+  Write-Host "##teamcity[buildProblem description='Version cannot be found: AssemblyInfo.Shared.cs file not found']"
+}
+$assemblyInfo = [IO.File]::ReadAllText($assemblyInfoPath)
+$assemblyInfo -match 'AssemblyVersion\("(?<major>[0-9]+).(?<minor>[0-9]+).(?<patch>[0-9]+).*"\)'
+
+$major = $matches['major']
+$minor = $matches['minor']
+$patch = $matches['patch']
+$buildNumber = "$major.$minor.$patch.%build.counter%"
+
+# Emit the BuildNumber to label the TeamCity build
+$teamCityBuildNumber = $buildNumber
+$assemblyInfo -match 'assembly:[ ]*Description\("(?<preReleaseInfo>[a-zA-Z0-9.]+)"\)'
+$preReleaseInfo = $matches['preReleaseInfo']
+If ($preReleaseInfo) {
+    # If beta.1 is supplied, semver the tc version as 2.2.0-beta1+45654 (http://semver.org/)
+    $teamCityBuildNumber = "$major.$minor.$patch-$preReleaseInfo+%build.counter%"
+}
+Write-Host "##teamcity[buildNumber '$teamCityBuildNumber']"
+
+
+# Update the assembly version in AssemblyInfo.Shared.cs
+# Note that we don't use TeamCity's "AssemblyInfo patcher" feature:
+# it runs right after GIT clone and we don't know the version at that point.
+$assemblyVersionPattern = 'AssemblyVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)'        
+$assemblyVersion = 'AssemblyVersion("' + $buildNumber + '")';        
+$assemblyFileVersionPattern = 'AssemblyFileVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)'  
+$assemblyFileVersion = 'AssemblyFileVersion("' + $buildNumber + '")'
+(Get-Content $assemblyInfoPath) | Foreach-Object {
+    $_ -replace $assemblyVersionPattern, $assemblyVersion `
+       -replace $assemblyFileVersionPattern, $assemblyFileVersion ` 
+    } | Set-Content $assemblyInfoPath -encoding UTF8 -force
+Write-Host "Updated AssemblyVersion/AssemblyFileVersion in '$assemblyInfoPath' to '$buildNumber'"
