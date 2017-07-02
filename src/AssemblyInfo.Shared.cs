cb47aa439e91d51b23a6c713eaf3dd6c1d44ffe4
Koen Jans <koen.jans@ipasoftware.com>
2017-07-02 12:21:15 +0200

    Initial structure

---
+++ src/AssemblyInfo.Shared.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("1.2.3")]
+[assembly: AssemblyFileVersion("1.2.3")]
+
+[assembly: AssemblyCompany("Company")]
+[assembly: AssemblyProduct("Company.eTools")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
---
+++ src/Desktop/Tests/eTools.Desktop.Tests/MainWindowTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace eTools.Desktop.Tests
+{
+
+    [TestFixture]
+    public class MainWindowTests
+    {
+
+        [Test]
+        public void Temp()
+        {
+            Assert.That(1 + 1, Is.EqualTo(2));
+        }
+    }
+}
